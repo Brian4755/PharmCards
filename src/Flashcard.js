1b7e33a64fc57a360852d496e261564bbe225ebc
Brian4755 <brian.mccune1@yahoo.com>
2023-08-21 06:30:14 -0500

    data display through useState and button click

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { prescriptionData } from "./prescription";
 
 const Flashcard = () => {
   const [prescription, setPrescription] = useState(0)
@@ -8,8 +9,16 @@ const Flashcard = () => {
   }
 
   return ( 
-    <div>
-      {prescription}
+    <div className="flashcard-data">
+      <div>
+        {prescriptionData[prescription].brand}
+      </div>
+      <div>
+        {prescriptionData[prescription].generic}
+      </div>
+      <div>
+        {prescriptionData[prescription].function}
+      </div>
       <button onClick={handleClick}>Click This</button>
     </div>
    );
